1a576922932313945745206e061b24d74d2ccd87
ulkermu <muratulker93@gmail.com>
2023-09-24 21:26:51 +0300

    Loading & Animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
+"use client";
+import Lottie from "lottie-react";
+import calenderAnimation from "@/assets/calender-employee.json";
+import { CustomNavLink } from "@/components";
+
 export default function Home() {
   return (
-    <main className="p-5 w-full">
-      <h1 className="gradient-text text-3xl font-bold text-center">
-        Track every trace of your employees
-      </h1>
-      
+    <main className="p-5 w-full max-w-[1280px] mx-auto">
+      <div className="flex g-5 justify-between max-[900px]:flex-col max-[900px]:justify-center">
+        <div className="flex flex-col justify-center g-5 min-w-[384px]">
+          <h1 className="gradient-text text-6xl font-bold text-center">
+            Track Leave Days
+          </h1>
+          <h3 className="my-2 text-3xl">
+            Track every trace of your employees' leave days and evasion. For
+            more efficient days, do not neglect to use us.
+          </h3>
+          <CustomNavLink
+            title="Explore"
+            href="/register"
+            containerStyles="px-6 py-3 text-xl bg-purple-500 hover:bg-purple-600 dark:bg-purple-400 dark:hover:bg-purple-500 w-fit mx-auto text-slate-200	dark:text-slate-800"
+          />
+        </div>
+        <Lottie
+          className="lottie max-w-xl max-[900px]:mx-auto"
+          animationData={calenderAnimation}
+        />
+      </div>
     </main>
   );
 }
--- components/CustomNavLink.tsx
+++ components/CustomNavLink.tsx
@@ -5,7 +5,7 @@ const CustomNavLink = ({ href, title, containerStyles }: CustomNavLinkProps) => 
   const cst = containerStyles === undefined ? "" : containerStyles
 
   return (
-    <Link className={`dark:hover:bg-slate-500 hover:bg-slate-100 ease-out duration-150 py-1 px-2 rounded-md font-bold ${cst}`} href={href}>{title}</Link>
+    <Link className={`dark:hover:bg-slate-500 hover:bg-slate-100 ease-out duration-300 py-1 px-2 rounded-md font-bold ${cst}`} href={href}>{title}</Link>
   )
 }
 
