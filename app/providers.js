d61899dcb86123b72a151c64511e941d26a145d2
ulkermu <muratulker93@gmail.com>
2023-09-26 00:08:30 +0300

    theme js to ts

--- app/providers.js
+++ app/providers.js
@@ -1,16 +1,15 @@
-"use client"
-import { ThemeProvider } from "next-themes"
-import { useEffect, useState } from "react"
-
+"use client";
+import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Providers({ children }) {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if (!mounted) return <>{children}</>
+  if (!mounted) return <>{children}</>;
 
   return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 }
